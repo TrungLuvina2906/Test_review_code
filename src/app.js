420619eff6d4d4bd95f00768506bcc07179bb03e
TrungLuvina2906 <nguyenductrung@luvina.net>
2026-03-18 17:12:55 +0700

    Apply bug fixes from automated code review

--- src/app.js
+++ src/app.js
@@ -1,1 +1,10 @@
-const express = require('express'); const app = express(); const bodyParser = require('body-parser'); const bookingsRouter = require('./routes/bookings'); app.use(bodyParser.json()); app.use('/api/bookings', bookingsRouter); app.listen(3000, () => { console.log('Server is running on port 3000'); });
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
