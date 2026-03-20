1fcfaab31208e0f35e1370c4caa624be6264aab3
TrungLuvina2906 <nguyenductrung@luvina.net>
2026-03-20 14:47:56 +0700

    Initial backend code generation from specification

---
+++ Infrastructure/Repositories/IRepository.cs
@@ -0,0 +1,8 @@
+public interface IRepository<T> where T : BaseEntity
+{
+    Task<T> GetByIdAsync(long id);
+    Task<List<T>> GetAllAsync();
+    Task AddAsync(T entity);
+    Task UpdateAsync(T entity);
+    Task DeleteAsync(long id);
+}
