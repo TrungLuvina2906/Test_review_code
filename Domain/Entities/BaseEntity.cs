940ac82b01d2f3499a16cdf70e6cfd0dc0b4b20e
TrungLuvina2906 <nguyenductrung@luvina.net>
2026-03-24 16:25:08 +0700

    Delete Domain/Entities directory

--- Domain/Entities/BaseEntity.cs
+++
@@ -1,6 +0,0 @@
-public abstract class BaseEntity
-{
-    public long Id { get; set; }
-    public DateTime CreatedAt { get; set; }
-    public DateTime? UpdatedAt { get; set; }
-}
--- Domain/Entities/PayrollPeriod.cs
+++
@@ -1,7 +0,0 @@
-public class PayrollPeriod : BaseEntity
-{
-    public string Code { get; set; } = default!;
-    public DateOnly DateFrom { get; set; }
-    public DateOnly DateTo { get; set; }
-    public PayrollStatus Status { get; set; }
-}
