7826919a3f7442bc562322a5db2b6f0c177ee18e
TrungLuvina2906 <nguyenductrung@luvina.net>
2026-03-24 16:25:18 +0700

    Delete Infrastructure directory

--- Infrastructure/Repositories/IRepository.cs
+++
@@ -1,8 +0,0 @@
-public interface IRepository<T> where T : BaseEntity
-{
-    Task<T> GetByIdAsync(long id);
-    Task<List<T>> GetAllAsync();
-    Task AddAsync(T entity);
-    Task UpdateAsync(T entity);
-    Task DeleteAsync(long id);
-}
